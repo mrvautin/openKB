mrvautin/openKB

commit 5a282d862cb8401d84113827ae1e89fdf1d0d1ed

Added ability to remove admin JS and CSS from theme layout.hbs

public/javascripts/openKB.js
@@ -19,7 +19,10 @@ $(document).ready(function(){
         $(this).addClass('table table-hover');
     });
 
-    $('.toggle-menu').jPushMenu();
+    // setup the push menu
+    if(typeof jPushMenu !== 'undefined'){
+        $('.toggle-menu').jPushMenu();
+    }
 
     // highlight any code blocks
     $('pre code').each(function(i, block){
@@ -27,7 +30,9 @@ $(document).ready(function(){
     });
 
 	// add the token field to the keywords input
-	$('#frm_kb_keywords').tokenfield();
+    if(typeof tokenfield !== 'undefined'){
+	    $('#frm_kb_keywords').tokenfield();
+    }
 
     if($('#editor').length){
         // setup editors

commit 6ea6600123da955b151c165fd6625fcb0e68b15e

Fixing what broke in 5a282d862cb8401d84113827ae1e89fdf1d0d1ed and remove accidental debut messages.

public/javascripts/openKB.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     });
 
     // setup the push menu
-    if(typeof jPushMenu !== 'undefined'){
+    if($('.toggle-menu').length){
         $('.toggle-menu').jPushMenu();
     }
 
@@ -30,8 +30,8 @@ $(document).ready(function(){
     });
 
 	// add the token field to the keywords input
-    if(typeof tokenfield !== 'undefined'){
-	    $('#frm_kb_keywords').tokenfield();
+    if($('#frm_kb_keywords').length){
+        $('#frm_kb_keywords').tokenfield();
     }
 
     if($('#editor').length){
@@ -140,10 +140,8 @@ $(document).ready(function(){
         })
         .done(function(msg){
             show_notification(msg, 'success', true);
-            console.log('success', msg.responseText);
         })
         .fail(function(msg){
-            console.log('fail', msg);
             show_notification(msg.responseText, 'danger');
         });
     });
@@ -157,10 +155,8 @@ $(document).ready(function(){
         })
         .done(function(msg){
             show_notification(msg, 'success', true);
-            console.log('success', msg.responseText, true);
         })
         .fail(function(msg){
-            console.log('fail', msg);
             show_notification(msg.responseText, 'danger');
         });
     });
@@ -246,8 +242,6 @@ function file_delete_confirm(img, id){
 function show_notification(msg, type, reload_page){
     // defaults to false
     reload_page = reload_page || false;
-
-    console.log(reload_page);
 
     $('#notify_message').removeClass();
     $('#notify_message').addClass('notify_message-' + type);