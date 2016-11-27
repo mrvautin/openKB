7db1e5fc98986718688a8425fc309dcfb018f990
ben <ben@unixben.com>
2016-11-27 20:45:50 +1100

    Locale (en) and app_context update

--- locales/en.js
+++ locales/en.js
@@ -152,5 +152,8 @@
 	"Access denied. Check password and try again.": "Access denied. Check password and try again.",
 	"Select a language": "Select a language",
 	"The language to use for public facing pages. Leave blank for default (English)": "The language to use for public facing pages. Leave blank for default (English)",
-	"Settings successfully updated.": "Settings successfully updated."
+	"Settings successfully updated.": "Settings successfully updated.",
+	"Results for": "Results for",
+	"No results found": "No results found",
+	"Access denied": "Access denied"
 }
