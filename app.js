08f68dd8a46f1f0a009f8bd26cbb8eed0e4d58e9
Kristoffer Gustafsson <gustafsson.kristoffer@outlook.com>
2017-07-29 10:42:14 +0200

    locales sv.js
    -Changed swe.js to sv.js to follow standard.
    -corrected app.js with sv instead of swe.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 
 // setup the translation
 var i18n = new (require('i18n-2'))({
-    locales: ['en', 'de', 'da', 'es', 'cn', 'ru', 'pt-br', 'jp', 'swe'],
+    locales: ['en', 'de', 'da', 'es', 'cn', 'ru', 'pt-br', 'jp', 'sv'],
     directory: path.join(__dirname, 'locales/'),
     defaultLocale: 'en',
     cookieName: 'locale'
