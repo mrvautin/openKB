2825e718d7a95dd4b5325d7364f5d4b16aae7db4
Tommi Savikko <dementik@gmail.com>
2017-07-29 22:52:23 +0300

    allow iframe tag with attributes youtube needs

--- public/javascripts/openKB.js
+++ public/javascripts/openKB.js
@@ -258,8 +258,11 @@ $(document).ready(function(){
         var cleanHTML = sanitizeHtml(fixed_html, {
             allowedTags: [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
                 'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br', 'div',
-                'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img' 
-            ]
+                'table', 'thead', 'caption', 'tbody', 'tr', 'th', 'td', 'pre', 'img', 'iframe'
+            ],
+            allowedAttributes: {
+                iframe: ['class', 'id', 'type', 'width', 'height', 'src', 'frameborder']
+            }
         });
 
         $('#preview').html(cleanHTML);
