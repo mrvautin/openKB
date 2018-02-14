8292d80a5111b77e71b422cc4ca318212d952fbb
Tal Avissar <Tal.Avissar@amdocs.com>
2018-02-14 15:46:38 +0200

    close connection with db close to mongo

--- app.js
+++ app.js
@@ -357,4 +357,32 @@ if(config.settings.database.type === 'embedded'){
     });
 }
 
+function exitHandler(options, err) {
+    if (options.cleanup) {
+        console.log('clean');
+        if(config.settings.database.type !== 'embedded'){
+            app.db.close();
+        }
+    }
+    if (err) {
+        console.log(err.stack);
+    }
+    if (options.exit) {
+        process.exit();
+    }
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
 module.exports = app;
